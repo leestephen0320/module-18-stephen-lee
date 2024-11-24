182a935cd385f75d5943568992c8aa5dc5bc7066
Stephen Lee <leestephen0320@gmail.com>
2024-11-24 15:49:47 -0800

    fixed file path

--- server/src/server.ts
+++ server/src/server.ts
@@ -5,7 +5,7 @@ import db from './config/connection.js'
 import { ApolloServer } from '@apollo/server';// Note: Import from @apollo/server-express
 import { expressMiddleware } from '@apollo/server/express4';
 import { typeDefs, resolvers } from './schemas/index.js';
-import { authenticateToken } from './utils/auth.js';
+import { authenticateToken } from './services/auth.js';
 import {fileURLToPath} from 'url';
 
 const __filename = fileURLToPath(import.meta.url);
