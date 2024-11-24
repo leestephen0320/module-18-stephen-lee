ea4f7261e24be15770e2e8e4da1599759352944d
Stephen Lee <leestephen0320@gmail.com>
2024-11-24 15:46:41 -0800

    fixed server.ts

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,36 +1,46 @@
 import express from 'express';
-import { ApolloServer } from '@apollo/server';
+import path from 'node:path';
+import type { Request, Response } from 'express';
+import db from './config/connection.js'
+import { ApolloServer } from '@apollo/server';// Note: Import from @apollo/server-express
 import { expressMiddleware } from '@apollo/server/express4';
-import path from 'path';
-
 import { typeDefs, resolvers } from './schemas/index.js';
-import db from './config/connection.js';
+import { authenticateToken } from './utils/auth.js';
+import {fileURLToPath} from 'url';
 
-const PORT = process.env.PORT || 3001;
-const app = express();
+const __filename = fileURLToPath(import.meta.url);
+
+// 👇️ "/home/john/Desktop/javascript"
+const __dirname = path.dirname(__filename);
+
 const server = new ApolloServer({
   typeDefs,
-  resolvers,
+  resolvers
 });
 
 const startApolloServer = async () => {
   await server.start();
-  
-  app.use(express.urlencoded({ extended: true }));
+  await db();
+
+  const PORT = process.env.PORT || 3001;
+  const app = express();
+
+  app.use(express.urlencoded({ extended: false }));
   app.use(express.json());
-  
-  app.use('/graphql', expressMiddleware(server));
 
-  // if we're in production, serve client/dist as static assets
+  app.use('/graphql', expressMiddleware(server as any,
+    {
+      context: authenticateToken as any
+    }
+  ));
+
   if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../client/dist')));
+    app.use(express.static(path.join(__dirname, '../../client/dist')));
 
-    app.get('*', (_req, res) => {
-      res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+    app.get('*', (_req: Request, res: Response) => {
+      res.sendFile(path.join(__dirname, '../../client/dist/index.html'));
     });
   }
-  
-  db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
   app.listen(PORT, () => {
     console.log(`API server running on port ${PORT}!`);
